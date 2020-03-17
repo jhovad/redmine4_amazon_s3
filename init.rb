9cfdd0e389ce75bfb2ef7ad614c81350fbbf25b1
Josef Hovad <josef@Josefs-MacBook-Pro.local>
2020-03-17 19:46:41 +0100

    init2

--- init.rb
+++ init.rb
@@ -5,7 +5,6 @@ Redmine::Plugin.register :amazon_s3 do
   name 'AmazonS3'
   version '0.0.1'
   description 'Use Amazon S3 as a storage engine for attachments'
-  url 'https://github.com/x3ro/redmine_amazon_s3'
+  url 'https://github.com/jhovad/redmine4_amazon_s3'
   author 'Josef Hovad'
-  author_url 'https://x3ro.de'
 end
